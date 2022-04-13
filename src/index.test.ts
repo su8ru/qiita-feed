8e974ffe107a841fdcb677d7b67bad65dc5928f7
subaru <s@su8ru.dev>
2022-04-13 15:40:31 +0900

    build: remove jest

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { app } from './index'
-
-describe('Test the application', () => {
-  it('should return 200 response', async () => {
-    const res = await app.request('http://localhost/')
-    expect(res.status).toBe(200)
-  })
-})
